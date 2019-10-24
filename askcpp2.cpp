af95f9a59066d81cba96ab05d5c1bcb4712412c9
iceQQ <42516265+iceQQ@users.noreply.github.com>
2019-10-24 11:41:09 +0300

    Add files via upload

--- askcpp2.cpp
+++ askcpp2.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main() {
 	string countries[n];
 	string abr[n];
-	long pop[n][57];
+	long long pop[n][57];
 	string line,word;
 	fstream fin;
 	fin.open("population-figures-easy.csv",ios::in);
@@ -29,9 +29,11 @@ int main() {
 			else if (k==1){
 				abr[i]=word;
 			}
-
+			else if (word == " "){
+				pop[i][k - 2] = 0;
+			}
 			else{
-				pop[i][k] = atol(word.c_str());
+				pop[i][k - 2] = atol(word.c_str());
 			}
 			k++;
 		}
@@ -39,12 +41,12 @@ int main() {
 	}
 	fin.close();
 	int j,av=0,ac=0;
-	float maxdif;
-    for(i=1;i<n;i++){
-        maxdif = (float)(pop[i][0]+pop[i][1])/(pop[i][0])*100;
+	double maxdif,maxdif2;
+    for(i=0;i<n;i++){
+        maxdif = (double)((pop[i][1]-pop[i][0])*100) /pop[i][0];
         for(j=0;j<56;j++){
-            if (maxdif < (float)(pop[i][j]+pop[i][j+1])/(pop[i][j])*100){
-                maxdif = (float)(pop[i][j]+pop[i][j+1])/(pop[i][j])*100;
+            if (maxdif < (double)((pop[i][j+1]-pop[i][j])*100) /pop[i][j]){
+                maxdif = (double)((pop[i][j+1]-pop[i][j])*100) /pop[i][j];
                 av=1960+j;
                 ac=1960+j+1;
             }
